68142d99ffd1a8f74888301bdd34c502694e5bb1
Paddle <PaddleStroke@users.noreply.github.com>
2023-11-03 18:54:27 +0100

    Translate DSH : A replacement for rectangular array.

--- src/Mod/Sketcher/Gui/Workbench.cpp
+++ src/Mod/Sketcher/Gui/Workbench.cpp
@@ -537,6 +537,7 @@ inline void SketcherAddWorkbenchTools<Gui::MenuItem>(Gui::MenuItem& consaccel)
               << "Sketcher_Rotate"
               << "Sketcher_Scale"
               << "Sketcher_Symmetry"
+              << "Sketcher_Translate"
               << "Sketcher_Clone"
               << "Sketcher_Copy"
               << "Sketcher_Move"
@@ -563,6 +564,7 @@ inline void SketcherAddWorkbenchTools<Gui::ToolBarItem>(Gui::ToolBarItem& consac
         << "Sketcher_Rotate"
         << "Sketcher_Scale"
         << "Sketcher_Symmetry"
+        << "Sketcher_Translate"
         << "Sketcher_CompCopy"
         << "Sketcher_RectangularArray"
         << "Sketcher_RemoveAxesAlignment";
