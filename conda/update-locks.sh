f6d634efeb6ed23d169afe1fb0ba1806195026c9
Jacob Oursland <jacob.oursland@gmail.com>
2024-05-13 10:55:25 -0600

    Conda: sort lock files.

--- conda/update-locks.sh
+++ conda/update-locks.sh
@@ -5,5 +5,5 @@ conda devenv --lock -f conda/environment.devenv.yml
 for platform in "linux" "osx" "win"
 do
     echo "@EXPLICIT" > conda/freecad.$platform-64.conda-lock.yml
-    cat conda/.freecad.$platform-64.conda-lock.yml | grep '^  url:' | awk '{print $2}' >> conda/freecad.$platform-64.conda-lock.yml
+    cat conda/.freecad.$platform-64.conda-lock.yml | grep '^  url:' | awk '{print $2}' | sort >> conda/freecad.$platform-64.conda-lock.yml
 done
