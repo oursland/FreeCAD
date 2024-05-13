69e4d8a7016dc6fc60cf8f4f727ce457409d0cee
Jacob Oursland <jacob.oursland@gmail.com>
2024-05-13 10:46:45 -0600

    Conda: update.

--- conda/update-locks.sh
+++ conda/update-locks.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# conda devenv --lock -f conda/environment.devenv.yml
+conda devenv --lock -f conda/environment.devenv.yml
 
 for platform in "linux" "osx" "win"
 do
-    cat conda/.freecad.$platform-64.conda-lock.yml | grep '^  url:' | awk '{print $2}' > conda/freecad.$platform-64.conda-lock.yml
+    echo "@EXPLICIT" > conda/freecad.$platform-64.conda-lock.yml
+    cat conda/.freecad.$platform-64.conda-lock.yml | grep '^  url:' | awk '{print $2}' >> conda/freecad.$platform-64.conda-lock.yml
 done
