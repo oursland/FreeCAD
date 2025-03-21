dc1f65eacffc3e59ce0033a4787afca88f3d84b1
Jacob Oursland <jacob.oursland@gmail.com>
2025-03-21 02:49:16 -0700

    WIP: rattler-build.

---
+++ package/rattler-build/build.bat
@@ -0,0 +1,34 @@
+@echo on
+
+:: free up extra disk space, compare
+:: https://github.com/conda-forge/conda-smithy/issues/1949
+rmdir /s /q C:\hostedtoolcache\windows
+
+set "CFLAGS= "
+set "CXXFLAGS= -DBOOST_PROGRAM_OPTIONS_DYN_LINK=1"
+set "LDFLAGS_SHARED= ucrt.lib"
+
+cmake ^
+    --preset conda-windows-release ^
+    -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
+    -D CMAKE_PREFIX_PATH:FILEPATH="%LIBRARY_PREFIX%" ^
+    -D CMAKE_LIBRARY_PATH:FILEPATH="%LIBRARY_PREFIX%/lib" ^
+    -D CMAKE_INSTALL_LIBDIR:FILEPATH="%LIBRARY_PREFIX%/lib" ^
+    -D CMAKE_INCLUDE_PATH:FILEPATH="%LIBRARY_PREFIX%/include" ^
+    -D INSTALL_TO_SITEPACKAGES:BOOL=ON ^
+    -D Python_EXECUTABLE:FILEPATH="%PYTHON%" ^
+    -D Python3_EXECUTABLE:FILEPATH="%PYTHON%" ^
+    -D FREECAD_USE_OCC_VARIANT="Official Version" ^
+    -D OCC_INCLUDE_DIR:FILEPATH="%LIBRARY_PREFIX%/include" ^
+    -D OCCT_CMAKE_FALLBACK:BOOL=ON ^
+    -B build ^
+    -S . ^
+    ${CMAKE_PLATFORM_FLAGS[@]}
+if %ERRORLEVEL% neq 0 exit 1
+
+ninja -C build install
+if %ERRORLEVEL% neq 0 exit 1
+
+rmdir /s /q "%LIBRARY_PREFIX%\doc"
+ren %LIBRARY_PREFIX%\bin\FreeCAD.exe freecad.exe
+ren %LIBRARY_PREFIX%\bin\FreeCADCmd.exe freecadcmd.exe
